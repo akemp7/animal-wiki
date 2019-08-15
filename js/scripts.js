68c88a5cb6e6195b224d24f7df130086b99e09dd
Anita Kemp and Jason Huels <anita@>
2019-08-15 10:04:09 -0700

    alternately use a swich case

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
 $(function() {
   $(".radio input").click(function() {
     var selected = $("input:radio[name=animal]:checked").val();
+    $(".dog").hide();
+    $(".shark").hide();
+    $(".bird").hide();
+//////////// Method 1 /////////////////////////
+    // if(selected === "dog") {
+    //   $(".dog").show();
+    // } else if(selected === "whaleshark") {
+    //   $(".shark").show();
+    // } else if(selected === "bowerbird") {
+    //   $(".bird").show();
+    // }
 
-    // $("div." + selected).show();
-    if(selected === "dog") {
-      $(".dog").show();
-      $(".shark").hide();
-      $(".bird").hide();
-    } else if(selected === "whaleshark") {
-      $(".shark").show();
-      $(".dog").hide();
-      $(".bird").hide();
-    } else if(selected === "bowerbird") {
-      $(".bird").show();
-      $(".shark").hide();
-      $(".dog").hide();
+//////////// Method 2 /////////////////////////
+    switch(selected) {
+      case "dog":
+        $(".dog").show();
+        break;
+      case "whaleshark":
+        $(".shark").show();
+        break;
+      case "bowerbird":
+        $(".bird").show();
+        break;
     }
 
   });
